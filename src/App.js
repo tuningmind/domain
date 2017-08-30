1288235528b0f7b6d3190821a06c64f6cee9c374
tuningmind <tuningmind@gmail.com>
2017-08-30 02:01:22 -0600

    Initial content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <canvas />
-        </p>
+      <div className='App'>
+        <header>
+        </header>
+        <main className='row-between wrap'>
+
+          <a href='http://greenfield.education/'>greenfield education</a>
+          <a href='http://tuningmind.github.io/carpe_diem/'>carpe diem</a>
+          <a href='https://tuningmind-tiy.github.io/interview-cake/'>interview cake</a>
+          <a href='https://tuningmind.github.io/electrix/'>electrix salon</a>
+          <a href='https://tuningmind-tiy.github.io/wk-01-betty/'>betty's</a>
+          <a href='https://tuningmind-tiy.github.io/hangman-wk-02/'>hangman</a>
+          <a href='https://tuningmind-tiy.github.io/tic-tac-toe/'>tic-tac-toe</a>
+          <a href='https://tuningmind-tiy.github.io/blog-fish/'>blogfish</a>
+          <a href='https://tuningmind-tiy.github.io/countdown-timer-wk-05/'>countdown</a>
+          <a href='https://tuningmind.github.io/mazeball/'>mazeball</a>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default App
