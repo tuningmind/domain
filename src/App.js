501f9cd1d4adb765af18204b31414f576637f286
tuningmind <tuningmind@gmail.com>
2018-03-04 19:28:25 -0700

    Add header

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <header>
-        </header>
         <main className='row-between wrap'>
-
-          <a href='http://greenfield.education/'>greenfield education</a>
-          <a href='http://tuningmind.github.io/carpe_diem/'>carpe diem</a>
-          <a href='https://tuningmind-tiy.github.io/interview-cake/'>interview cake</a>
-          <a href='https://tuningmind.github.io/electrix/'>electrix salon</a>
-          <a href='https://tuningmind-tiy.github.io/wk-01-betty/'>betty's</a>
-          <a href='https://tuningmind-tiy.github.io/hangman-wk-02/'>hangman</a>
-          <a href='https://tuningmind-tiy.github.io/tic-tac-toe/'>tic-tac-toe</a>
-          <a href='https://tuningmind-tiy.github.io/blog-fish/'>blogfish</a>
-          <a href='https://tuningmind-tiy.github.io/countdown-timer-wk-05/'>countdown</a>
-          <a href='https://tuningmind.github.io/mazeball/'>mazeball</a>
+          <div name="typewriter"  className='typerwriter-animation'>
+			<div className='header'>tuningmind.net</div>
+          </div>
+          <div className='column-left'>
+			<a href='http://greenfield.education/'>greenfield education</a>
+			<a href='http://tuningmind.github.io/carpe_diem/'>carpe diem</a>
+			<a href='https://tuningmind-tiy.github.io/interview-cake/'>interview cake</a>
+			<a href='https://tuningmind.github.io/electrix/'>electrix salon</a>
+			<a href='https://tuningmind-tiy.github.io/wk-01-betty/'>betty's</a>
+			<a href='https://tuningmind-tiy.github.io/hangman-wk-02/'>hangman</a>
+			<a href='https://tuningmind-tiy.github.io/tic-tac-toe/'>tic-tac-toe</a>
+			<a href='https://tuningmind-tiy.github.io/blog-fish/'>blogfish</a>
+			<a href='https://tuningmind.github.io/mazeball/'>mazeball</a>
+		  </div>
         </main>
       </div>
     );
